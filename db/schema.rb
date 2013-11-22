e59535e85b6255437cef4a2f2e8685da3abdc2b4
Puja Patel <pujapatel85@gmail.com>
2013-11-22 03:01:15 +0000

    Fixed my user bug

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131115023020) do
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "string"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
